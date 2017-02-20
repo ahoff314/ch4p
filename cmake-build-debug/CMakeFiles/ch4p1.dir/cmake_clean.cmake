cfde6a1a76f6ab1e17433fdbb0cdb76eb196c81a
ahoff314 <alexanderjhoffmann@gmail.com>
2017-02-20 10:54:20 -0700

    First commit

---
+++ cmake-build-debug/CMakeFiles/ch4p1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ch4p1.dir/main.cpp.o"
+  "ch4p1.pdb"
+  "ch4p1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ch4p1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,52 @@
+/***************************
+ * Alex Hoffmann
+ * 2/22/17
+ * Chapter 4 Program
+ ****************************/
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int totalCookies;
+    int boxesNeeded;
+    int leftoverCookies;
+    int containersNeeded;
+    int leftoverBoxes;
+
+    cout << "Please enter the total number of cookies: " << endl;
+
+    //Receive totalCookes input
+    cin >> totalCookies;
+
+    //Determine boxes needed, calculate leftover cookies
+    if (totalCookies >= 24 ) {
+        boxesNeeded = totalCookies / 24;
+        leftoverCookies = totalCookies % 24;
+    }
+    else
+        leftoverCookies = totalCookies;
+
+
+    //Determine containers needed, calculate leftover boxes
+    if (boxesNeeded >= 75){
+        containersNeeded = boxesNeeded / 75;
+        leftoverBoxes = boxesNeeded % 75;
+    }
+    else
+        leftoverBoxes = boxesNeeded;
+
+
+    //Output number boxes needed, leftover cookies, number of containers needed, leftover boxes
+    cout << "The number of cookie boxes needed to hold the cookies: " << boxesNeeded << endl;
+    cout << "Leftover cookies: " << leftoverCookies << endl;
+    cout << "The number of containers needed to store the cookie boxes: " << containersNeeded << endl;
+    cout << "Leftover boxes: " << leftoverBoxes << endl;
+
+
+    return 0;
+}
+
